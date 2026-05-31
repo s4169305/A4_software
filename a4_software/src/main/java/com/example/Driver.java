3d669eee9f97078a161031258f7f92b9088f4d1b
Zeynab Ahmedreza Dawood <s4168394@student.rmit.edu.au>
2026-05-31 19:00:38 +1000

    driver.java

--- a4_software/src/main/java/com/example/Driver.java
+++ a4_software/src/main/java/com/example/Driver.java
@@ -7,5 +7,38 @@ public class Driver {
     private String licenseType; // Light, Medium, Heavy, PublicTransport
     private String address;
     private String birthdate;
+
+    public Driver(String driverID, String name, int experienceYears, String licenseType, String address, String birthdate) {
+        this.driverID = driverID;
+        this.name = name;
+        this.experienceYears = experienceYears;
+        this.licenseType = licenseType;
+        this.address = address;
+        this.birthdate = birthdate;
+    }
+
+    public String getDriverID() {
+        return driverID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
 }
 
