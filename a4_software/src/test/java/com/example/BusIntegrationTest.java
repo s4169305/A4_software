f552b6b5173266da8509663cacaa5bb968de05d5
Kirsten Claire Canda <s4169305@student.rmit.edu.au>
2026-06-01 09:35:53 +1000

    Changed test file names to suit assignment specs

---
+++ a4_software/src/test/java/com/example/BusIntegrationTest.java
@@ -0,0 +1,22 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BusIntegrationTest {
+    @Test
+    void shouldStoreValidBus() {
+        BusRepository repo = new BusRepository();
+        Bus bus = new Bus("B1", 50, 75.0, "Diesel");
+
+        repo.add(bus);
+
+        assertEquals(1, repo.count());
+        assertNotNull(repo.findById("B1"));
+
+    }
+}
+
+// add
+// update
+// count
+// retrieve
