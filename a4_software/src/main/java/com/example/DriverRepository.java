84a2af64961d22eeb5a1304cac10db7ed0dded8e
Kirsten Claire Canda <s4169305@student.rmit.edu.au>
2026-05-31 19:21:58 +1000

    Created txt files and started on BusRepoTest

--- a4_software/src/main/java/com/example/DriverRepository.java
+++ a4_software/src/main/java/com/example/DriverRepository.java
@@ -1,5 +1,19 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+// Importing array and list
+
 public class DriverRepository {
-// Add (), Update (), Retrieve (), Count () functions
+// Add () 
+    private final List<Driver> drivers = new ArrayList<>();
+
+    public void add(Driver driver) {
+        drivers.add(driver);
+    }
+
+
+// Update ()
+// Retrieve ()
+// Count () functions
 }
