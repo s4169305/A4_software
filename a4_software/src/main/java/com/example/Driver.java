3338957491c0804c158e28cdc73c814de37c391f
Zeynab Ahmedreza Dawood <s4168394@student.rmit.edu.au>
2026-05-31 21:30:47 +1000

    driver.java 2

--- a4_software/src/main/java/com/example/Driver.java
+++ a4_software/src/main/java/com/example/Driver.java
@@ -1,6 +1,8 @@
 package com.example;
 
+// Driver class stores driver information 
 public class Driver {
+    // Driver attributes
     private String driverID;
     private String name;
     private int experienceYears;
@@ -8,6 +10,7 @@ public class Driver {
     private String address;
     private String birthdate;
 
+    // Consturcter to initialize driver attributes
     public Driver(String driverID, String name, int experienceYears, String licenseType, String address, String birthdate) {
         this.driverID = driverID;
         this.name = name;
@@ -17,28 +20,35 @@ public class Driver {
         this.birthdate = birthdate;
     }
 
+    // Returns the driver ID
     public String getDriverID() {
         return driverID;
     }
 
+    // Returns the driver name
     public String getName() {
         return name;
     }
 
+    // Returns the driver experience years
     public int getExperienceYears() {
         return experienceYears;
     }
 
+    // Returns the driver license type
     public String getLicenseType() {
         return licenseType;
     }
 
+    // Returns the  address
     public String getAddress() {
         return address;
     }
 
+    // Returns the birthdate
     public String getBirthdate() {
         return birthdate;
     }
+    
 }
 
