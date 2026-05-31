7eca7593128ec6cc7ce782e7bd201af692c25472
Kirsten Claire Canda <s4169305@student.rmit.edu.au>
2026-05-31 19:36:16 +1000

    test

--- a4_software/src/test/java/com/example/BusRepositoryTest.java
+++ a4_software/src/test/java/com/example/BusRepositoryTest.java
@@ -2,16 +2,16 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BusRepositoryTest {
-    @Test
-    void shouldStoreValidBus() {
-        BusRepository repo = new BusRepository();
-        Bus bus = new Bus("B1", 50, 75.0, "Diesel");
+//public class BusRepositoryTest {
+//     @Test
+//     void shouldStoreValidBus() {
+//         BusRepository repo = new BusRepository();
+//         Bus bus = new Bus("B1", 50, 75.0, "Diesel");
 
-        repo.add(bus);
+//         repo.add(bus);
 
-        assertEquals(1, repo.count());
-        assertNotNull(repo.findById("B1"));
+//         assertEquals(1, repo.count());
+//         assertNotNull(repo.findById("B1"));
 
-    }
-}
+//     }
+// }
