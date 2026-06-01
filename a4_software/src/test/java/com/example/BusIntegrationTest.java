8b42876ea6ec5b26011c16b961ef20a69641806b
Kirsten Claire Canda <s4169305@student.rmit.edu.au>
2026-06-01 16:10:43 +1000

    Added BusDataLoader and started on BusIntegrationTest2

--- a4_software/src/test/java/com/example/BusIntegrationTest.java
+++ a4_software/src/test/java/com/example/BusIntegrationTest.java
@@ -9,49 +9,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BusIntegrationTest {
 //checking if buses are valid
-    @Test
-    void shouldStoreValidBus() throws IOException {  // testing if a bus is added successfully
-        Bus bus = BusDataLoader.loadFromFile(Path.of("src/main/java/com/example/testData/BusData.txt")); // adding a new bus from BusDataLoader
-        BusRepository repo = new BusRepository();
+    // @Test
+    // void shouldStoreValidBus() throws IOException {  // testing if a bus is added successfully
+    //     Bus bus = BusDataLoader.loadFromFile(Path.of("src/main/java/com/example/testData/BusData.txt")); // adding a new bus from BusDataLoader
+    //     BusRepository repo = new BusRepository();
 
-        repo.add(bus);
+    //     repo.add(bus);
 
-        assertEquals(1, repo.count()); // checks if the amount in repo is equal to 1, meaning that the bus has been added
-        assertNotNull(repo.retrieve(bus.getBusID())); // bus can be retrieved
-        assertEquals(bus.getBusID(), repo.retrieve(bus.getBusID()).getBusID()); // the correct bus is stored
-    }
+    //     assertEquals(1, repo.count()); // checks if the amount in repo is equal to 1, meaning that the bus has been added
+    //     assertNotNull(repo.retrieve(bus.getBusID())); // bus can be retrieved
+    //     assertEquals(bus.getBusID(), repo.retrieve(bus.getBusID()).getBusID()); // the correct bus is stored
+    // }
 
-    // rejecting duplicate buses
-    @Test
-    void shouldRejectDuplicateBusId() throws IOException {
-        BusRepository repo = new BusRepository();
-        Bus bus1 = BusDataLoader.loadFromFile(Path.of("src/main/java/com/example/testData/BusData.txt")); // adding a new bus from BusDataLoader
-        Bus bus2 = new Bus (bus1.getBusID(), 40, 60.0, "Hybrid"); // test bus
+    // // rejecting duplicate buses
+    // @Test
+    // void shouldRejectDuplicateBusId() throws IOException {
+    //     BusRepository repo = new BusRepository();
+    //     Bus bus1 = BusDataLoader.loadFromFile(Path.of("src/main/java/com/example/testData/BusData.txt")); // adding a new bus from BusDataLoader
+    //     Bus bus2 = new Bus (bus1.getBusID(), 40, 60.0, "Hybrid"); // test bus
 
-        repo.add(bus1);
+    //     repo.add(bus1);
 
-        assertThrows(IllegalArgumentException.class, () -> repo.add(bus2));
-        assertEquals(1, repo.count());
-    }
+    //     assertThrows(IllegalArgumentException.class, () -> repo.add(bus2));
+    //     assertEquals(1, repo.count());
+    // }
 
-     // rejecting bus IDs are not 8 digits long
-    @Test
-    void shouldRejectBusIdThatIsNot8Digits() throws IOException {
-        BusRepository repo = new BusRepository();
-        Bus invalid = BusDataLoader.loadFromFile(Path.of("src/main/java/com/example/testData/InvalidBusData.txt"));
+    //  // rejecting bus IDs are not 8 digits long
+    // @Test
+    // void shouldRejectBusIdThatIsNot8Digits() throws IOException {
+    //     BusRepository repo = new BusRepository();
+    //     Bus invalid = BusDataLoader.loadFromFile(Path.of("src/main/java/com/example/testData/InvalidBusData.txt"));
 
-        assertThrows(IllegalArgumentException.class, () -> repo.add(invalid));
-        assertEquals(0, repo.count());
-    }
+    //     assertThrows(IllegalArgumentException.class, () -> repo.add(invalid));
+    //     assertEquals(0, repo.count());
+    // }
 
-    // rejecting bus IDs that have non-digits
-    @Test
-    void shouldRejectBusIdWithNonDigitCharacters() throws IOException {
-        BusRepository repo = new BusRepository();
-        Bus invalid = BusDataLoader.loadFromFile(Path.of("src/main/java/com/example/testData/InvalidBusData.txt"));
+    // // rejecting bus IDs that have non-digits
+    // @Test
+    // void shouldRejectBusIdWithNonDigitCharacters() throws IOException {
+    //     BusRepository repo = new BusRepository();
+    //     Bus invalid = BusDataLoader.loadFromFile(Path.of("src/main/java/com/example/testData/InvalidBusData.txt"));
 
-        assertThrows(IllegalArgumentException.class, () -> repo.add(invalid));
-    }
+    //     assertThrows(IllegalArgumentException.class, () -> repo.add(invalid));
+    // }
 
 
 
